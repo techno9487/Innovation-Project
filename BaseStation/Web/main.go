98c2e7f35ffb49cec622cf96f50466ca80031f15
Toby Dunn <tobythedude99@gmail.com>
2018-02-15 19:14:55 +0000

    added db connection

--- BaseStation/Web/main.go
+++ BaseStation/Web/main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 
 	"html/template"
 
 	"github.com/gorilla/mux"
 )
 
+var globalDB *sql.DB
+
 func main() {
+
+	initDB()
+	defer globalDB.Close()
 
 	router := mux.NewRouter()
 
@@ -19,11 +27,23 @@ func main() {
 	log.Fatal(http.ListenAndServe("0.0.0.0:8080", nil))
 }
 
+func initDB() {
+	db, err := sql.Open("mysql", "root:password@/project?parseTime=true")
+	if err != nil {
+		log.Fatal(err)
+	}
+	globalDB = db
+
+}
+
 func homeHandler(res http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("views/index.html")
 	if err != nil {
 		log.Println(err)
 	}
 
-	t.Execute(res, nil)
+	data := map[string]interface{}{}
+	data["names"] = fetchRooms()
+
+	t.Execute(res, data)
 }
