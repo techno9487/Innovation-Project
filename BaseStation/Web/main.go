834b9f1d3bfd653a78a78129c017b7a58a84e2dd
Toby Dunn <tobythedude99@gmail.com>
2018-02-15 15:05:22 +0000

    Started basic web app

--- BaseStation/Web/main.go
+++ BaseStation/Web/main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"html/template"
+
+	"github.com/gorilla/mux"
+)
+
 func main() {
 
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", homeHandler)
+
+	http.Handle("/", router)
+	log.Fatal(http.ListenAndServe("0.0.0.0:8080", nil))
+}
+
+func homeHandler(res http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("views/index.html")
+	if err != nil {
+		log.Println(err)
+	}
+
+	t.Execute(res, nil)
 }
