e62d616fabf2f3e32b2f021214b0232ba41773cc
Toby Dunn <tobythedude99@gmail.com>
2018-05-03 16:13:30 +0100

    encodes key to bse64

--- BaseStation/Sync/sync.cpp
+++ BaseStation/Sync/sync.cpp
@@ -1,5 +1,8 @@
 #include <stdio.h>
+#include <string.h>
 #include <openssl/rand.h>
+#include <openssl/bio.h>
+#include <openssl/evp.h>
 
 /*
     Author: Toby Dunn
@@ -8,16 +11,22 @@
     here wireless informtion and encryption keys are exchanged
 */
 
+//Key size for the program to use
+#define KEY_SIZE 16
+
 //Holds data for a generated key
 struct KeyData_t
 {
-    unsigned char key[16];
-    unsigned char iv[16];
+    unsigned char key[KEY_SIZE];
+    unsigned char iv[KEY_SIZE];
 };
 
 //Generates a key and IV and stores the information in the
 //structure passed as a pointer
 int GenerateKey(KeyData_t* data);
+
+//Encodes the given key to base64
+int EncodeKey(unsigned char* key,unsigned char* output,int output_length);
 
 int main()
 {
@@ -27,7 +36,15 @@ int main()
         printf("Failed to generate key\n");
         return 1;
     }
-    
+
+    unsigned char encoded_key[25] = {};
+    if(!EncodeKey(data.key,encoded_key,25))
+    {
+        printf("Failed to encode key\n");
+        return -1;
+    }
+
+    printf("%s\n",encoded_key);
 
     return 0;
 }
@@ -35,18 +52,43 @@ int main()
 int GenerateKey(KeyData_t* data)
 {
     //Generate a random key
-    if(!RAND_bytes(data->key,16))
+    if(!RAND_bytes(data->key,KEY_SIZE))
     {
         printf("Failed to generate device key\n");
         return -1;
     }
 
     //Generate a random IV
-    if(!RAND_bytes(data->iv,16))
+    if(!RAND_bytes(data->iv,KEY_SIZE))
     {
         printf("Failed to generate IV\n");
         return -1;
     }
+
+    return 1;
+}
+
+int EncodeKey(unsigned char* key,unsigned char* encoded,int output_length)
+{
+    BIO* b64 = BIO_new(BIO_f_base64());
+    BIO* output = BIO_new(BIO_s_mem());
+    BIO_set_close(output,BIO_CLOSE);
+    BIO_push(b64,output);
+
+    BIO_write(b64,key,KEY_SIZE);
+    BIO_flush(b64);
+
+    char *pBuf;
+    long length = BIO_get_mem_data(output,&pBuf);
+    
+    if(output_length < length)
+    {
+        return 0;
+    }
+
+    memcpy(encoded,pBuf,length-1);
+        
+    BIO_free_all(b64);
 
     return 1;
 }
