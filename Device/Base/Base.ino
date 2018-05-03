8ca8e143a4c123176c7c860b679152900f70cd78
Toby Dunn <tobythedude99@gmail.com>
2018-05-03 14:34:52 +0100

    removed unused code

--- Device/Base/Base.ino
+++ Device/Base/Base.ino
@@ -3,22 +3,21 @@
  * Purpose: Base class for any IoT device in our smart home network
  */
 
-#include <EEPROM.h>
+#include <ESP8266WiFi.h>
+#include <ArduinoJson.h>
 
-/**
- * Called when the device needs to reset back to "Factory" settings
- */
- void ResetDevice()
- {
+#define RELAY_PIN 0
+
+//Buffer for all packets being received?
+char packetBuffer[1024];
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
   
- }
-
-void setup() {
-  // put your setup code here, to run once:
-
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}
