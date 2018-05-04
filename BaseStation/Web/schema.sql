f3ef0e5b94920993fd251de91da383a5f9b1d6e1
Toby Dunn <tobythedude99@gmail.com>
2018-05-04 20:36:12 +0100

    plz work

--- BaseStation/Web/schema.sql
+++ BaseStation/Web/schema.sql
@@ -1,15 +1,8 @@
-CREATE TABLE Room (
-    Id int NOT NULL primary key AUTO_INCREMENT,
-    RoomName VARCHAR(255)
-);
-
 CREATE TABLE Device (
     Id int NOT NULL primary key AUTO_INCREMENT,
     DeviceName varchar(255),
-    DeviceKey BLOB,
-    DeviceMeta BLOB,
-    RoomId int,
-    FOREIGN KEY (RoomId) REFERENCES Room(Id)
+    DeviceKey varchar(255),
+    DeviceIV varchar(255)
 );
 
 CREATE TABLE Log (
